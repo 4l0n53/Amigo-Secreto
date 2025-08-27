c044fb07a38867a3328a9ca4b39ebf77cd80a50e
Jairo Henao <jairohenaor@gmail.com>
2025-08-27 01:23:43 -0400

    actulización en la validación

--- app.js
+++ app.js
@@ -12,27 +12,55 @@ function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value;
     
     //Valida que el campo no esté vacío
-    if (nombreAmigo.trim() === "") {
-        asignarTextoElemento('h2', 'Debe ingresar un nombre.');
+    if (nombreAmigo === "") {
+         Swal.fire({
+            position: "center",
+            icon: "warning",
+            title: "Debe ingresar un nombre",
+            showConfirmButton: false,
+            timer: 3000
+        }); 
+  
         return; //Detiene la ejecución si el campo está vacío
+        
     }
 
     //Valida que el nombre solo contenga letras y espacios
     const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
     if (!regex.test(nombreAmigo)) {
-        asignarTextoElemento('h2', 'El nombre solo puede contener letras.');
+            Swal.fire({
+            position: "center",
+            icon: "warning",
+            title: "El nombre solo puede contener letras",
+            showConfirmButton: false,
+            timer: 3000
+        }); 
+     
         return; //Detiene la ejecución si la validación falla
     }
 
      //Validar que el nombre no exista ya en la lista
     const yaExiste = listaDeAmigos.some(nombreExistente => nombreExistente.toLowerCase() === nombreAmigo.toLowerCase());
     if (yaExiste) {
-    asignarTextoElemento('h2', 'Ese nombre ya ha sido agregado.');
+         Swal.fire({
+            position: "center",
+            icon: "warning",
+            title: "Ese nombre ya ha sido agregado",
+            showConfirmButton: false,
+            timer: 3000
+        }); 
+ 
     return; //Detiene la ejecución si el nombre ya esta en la lista
     }
 
     //Si todo es correcto, agrega el amigo a la lista y actualiza la interfaz
-    asignarTextoElemento('h2', '¡Nombre agregado!');
+    Swal.fire({
+        title: "¡Nombre agregado!",
+        icon: "success",
+        draggable: true,
+        timer: 3000
+    });
+      
     listaDeAmigos.push(nombreAmigo);
     lista(); //Llama a tu función para mostrar la lista de amigos
 
@@ -57,17 +85,38 @@ function lista (){
         asignarTextoElemento('#listaAmigos',contenido);
 }
 
+let campoDeEntrada = document.getElementById('amigo');
+
+// Agregar un "event listener" que detecta la pulsación de una tecla
+campoDeEntrada.addEventListener("keydown", function(event) {
+    // Verificar si la tecla presionada es "Enter"
+    if (event.key === "Enter" || event.key === 13) {
+        // Evitar el comportamiento predeterminado del formulario
+        event.preventDefault(); 
+        
+        // Llamar a la función que agrega al amigo
+        agregarAmigo();
+    }
+});
+
 //Sortea aleatoreamente los nombres almacenados en el arreglo
 function sortearAmigo(){
     //Genera un resultado aleatorio con los elementos del arreglo
     let sorteo = Math.floor(Math.random()*listaDeAmigos.length);
     //Valida que el arreglo tenga datos para hecer la operación
     if (listaDeAmigos == ""){
-        asignarTextoElemento('h2', 'Por favor ingrese un nombre antes de Sortear!!')
+       Swal.fire({
+            position: "center",
+            icon: "warning",
+            title: "Debe ingresar un nombre",
+            showConfirmButton: false,
+            timer: 3000
+        });
     }
     //Realiza el sorteo y imprime el resultado en pantalla
     else {
     asignarTextoElemento('#resultado', `Tu amigo secreto es: ${(listaDeAmigos[sorteo])}`);
+    timer: 3000
     //Elimina los datos del arreglo y limpia la lista
     limpiarLista();
     }
